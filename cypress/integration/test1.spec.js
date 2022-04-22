7c1d58489b0e28ac8dedb5dd1701e9772690abb5
Piyaluk <piyaluk.tu37@mail.kmutt.ac.th>
2022-04-22 15:57:27 +0700

    no change

--- cypress/integration/test1.spec.js
+++ cypress/integration/test1.spec.js
@@ -1,2 +1,8 @@
 describe('' , () => {
+  it('', () => {
+    cy.visit('https://www.github.com')
+    cy.get('.octicon-three-bars').click()
+    cy.get('.mr-0:nth-child(1) .HeaderMenu-summary').click()
+    cy.get('.mr-0:nth-child(1) li:nth-child(2) > .lh-condensed-ultra').click()
+  })
 })
