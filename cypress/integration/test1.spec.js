1b9421c69a321ec3d36a49edd3ad6e1c6996623a
massjj <sasikarn.a@mail.kmutt.ac.th>
2022-05-06 00:58:13 +0700

    [fixed] webview selector
    fixed one click to create only one block

--- cypress/integration/test1.spec.js
+++ cypress/integration/test1.spec.js
@@ -1,12 +1,2 @@
-describe('MakeTest test' , () => {
-  it('JPet store', () => {
-    cy.visit('https://petstore.octoperf.com/actions/Catalog.action')
-    cy.get('#SidebarContent > a:nth-child(1) > img').click()
-    cy.get('tr:nth-child(2) a').click()
-    cy.get('h2').should('be.contain', 'Angelfish')
-    cy.get('tr:nth-child(2) .Button').click()
-    cy.get('tbody > :nth-child(2) > :nth-child(3)').should('be.contain', 'Large Angelfish')
-    cy.get('.Button:nth-child(1)').click()
-    cy.get('td:nth-child(3)').should('not.be.visible')
-  })
+describe('' , () => {
 })
