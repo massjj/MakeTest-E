7b77955bebfd1f6c2295bf621225bd013f5406a5
Piyaluk <piyaluk.tu37@mail.kmutt.ac.th>
2022-05-19 17:37:33 +0700

    nochange

--- cypress/integration/buffer.spec.js
+++ cypress/integration/buffer.spec.js
@@ -1,5 +1,9 @@
 describe('' , () => {
+<<<<<<< Updated upstream
   it('', () => {
     expect(0).to.equal(0)
+=======
+  it('', () => {  cy.log((string.length('gg')))
+>>>>>>> Stashed changes
   })
 })
--- cypress/integration/test1.spec.js
+++ cypress/integration/test1.spec.js
@@ -1,4 +1,8 @@
 describe('' , () => {
   it('', () => {
+    var num = ["dd","ss","ff","gg"]
+    var num2 = num.splice(2,1)
+    num.reverse()
+    console.log(num)
   })
 })
